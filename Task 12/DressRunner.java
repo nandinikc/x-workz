f5285f258aaf13e8e6c43384391fa8c90be800c6
nandinikc <118906685+nandinikc@users.noreply.github.com>
2023-06-28 19:06:27 +0530

    Add files via upload

---
+++ Task 12/DressRunner.java
@@ -0,0 +1,171 @@
+class DressRunner
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Running main in DressRunner class");
+		//class Dress
+		Dress ref1=new Dress();
+		Dress ref2=new Dress();
+		Dress temp=new Dress();
+		Dress temp1=new Dress();
+		Dress ref3=new Dress();
+		Dress ref4=new Dress();
+		Dress ref5=new Dress();
+		Dress ref6=new Dress();
+		Dress ref7=new Dress();
+		Dress ref8=new Dress();
+		Dress ref9=new Dress();
+		Dress temp4=new Dress();
+		Dress temp5=new Dress();
+		Dress temp6=new Dress();
+		Dress temp7=new Dress();
+		//class Watch
+		Watch watch1=new Watch();
+		Watch watch2=new Watch();
+		Watch watch3=new Watch();
+		Watch watch4=new Watch();
+		Watch watch5=new Watch();
+		Watch watch6=new Watch();
+		Watch watch7=new Watch();
+		Watch watch8=new Watch();
+		Watch watch9=new Watch();
+		Watch watch10=new Watch();
+		Watch watch11=new Watch();
+		Watch watch12=new Watch();
+		Watch watch13=new Watch();
+		Watch watch14=new Watch();
+		Watch watch15=new Watch();
+		//class Box
+		Box box1=new Box();
+		Box box2=new Box();
+		Box box3=new Box();
+		Box box4=new Box();
+		Box box5=new Box();
+		Box box6=new Box();
+		Box box7=new Box();
+		Box box8=new Box();
+		Box box9=new Box();
+		Box box10=new Box();
+		Box box11=new Box();
+		Box box12=new Box();
+		Box box13=new Box();
+		Box box14=new Box();
+		Box box15=new Box();
+		//class Chocolate
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		new Chocolate();
+		//class Ball
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		new Ball();
+		//class Basket
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		new Basket();
+		//class Mirror
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		new Mirror();
+		//class Umbrella
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		new Umbrella();
+		//class Elephant
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		new Elephant();
+		//class Aeroplane
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		new Aeroplane();
+		
+		
+		
+		
+	}
+}
