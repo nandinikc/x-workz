f01b5ec51f27bfb142ce5c186b093463aa0c05d3
nandinikc <118906685+nandinikc@users.noreply.github.com>
2023-06-28 19:37:04 +0530

    Add files via upload

---
+++ june 28/Dress.java
@@ -0,0 +1,70 @@
+class Dress
+{
+	Dress()
+	{
+		System.out.println("I have many dresses in my home..");
+	}
+}
+class Watch
+{
+	Watch()
+	{
+		System.out.println("My watch is not working...");
+	}
+}
+class Box
+{
+	Box()
+	{
+		System.out.println("Can you bring my box");
+	}
+}
+class Chocolate
+{
+	Chocolate()
+	{
+		System.out.println("I like KitKat");
+	}
+}
+class Ball
+{
+	Ball()
+	{
+		System.out.println("I have 2 White balls and 3 green balls");
+	}
+}
+class Basket
+{
+	Basket()
+	{
+		System.out.println("Basket full of lilly flowers");
+	}
+}
+class Mirror
+{
+	Mirror()
+	{
+		System.out.println("Mirror images are original..");
+	}
+}
+class Umbrella
+{
+	Umbrella()
+	{
+		System.out.println("I have one umbrella");
+	}
+}
+class Elephant
+{
+	Elephant()
+	{
+		System.out.println("Many elephants in zoo");
+	}
+}
+class Aeroplane
+{
+	Aeroplane()
+	{
+		System.out.println("Aeroplane is flying in the sky....");
+	}
+}
